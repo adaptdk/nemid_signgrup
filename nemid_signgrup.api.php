49187ff5a48d713a131c08cd986f9b15072c7a7e
Mikkel Jakobsen <mikkel@adapt.dk>
2015-10-29 22:38:36 +0100

    Adding hook documentation

---
+++ nemid_signgrup.api.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @file
+ * NemID Signaturgruppen API.
+ */
+
+/**
+ * Callback if NemID authentication succeeded.
+ * @param object $nemid_info
+ */
+function hook_nemid_signgrup_callback_success($nemid_info) {
+
+}
+
+/**
+ * Callback if NemID authentication failed.
+ */
+function hook_nemid_signgrup_callback_failure() {
+
+}
+
+/**
+ * Callback if user cancelled NemID authentication.
+ */
+function hook_nemid_signgrup_callback_user_cancel() {
+
+}
